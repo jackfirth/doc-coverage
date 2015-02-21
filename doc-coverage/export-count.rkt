49e64893cc0aa58b7542e58cd2434d5e7ba92616
JackFirth <jackhfirth@gmail.com>
2015-02-20 23:46:56 -0800

    Fix documentation statistics functions

--- doc-coverage/export-count.rkt
+++ doc-coverage/export-count.rkt
@@ -2,10 +2,15 @@
 
 (require "export-lists.rkt")
 
+(provide module-num-exports
+         module-num-documented-exports
+         module-num-undocumented-exports
+         module-documentation-ratio)
+
 (define module-num-exports (compose length module->all-exported-names))
 (define module-num-documented-exports (compose length module->documented-exported-names))
 (define module-num-undocumented-exports (compose length module->undocumented-exported-names))
 
-(define (module-documentation% mod)
+(define (module-documentation-ratio mod)
   (/ (module-num-documented-exports mod)
-     (module-num-undocumented-exports mod)))
+     (module-num-exports mod)))
