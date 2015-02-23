d2af694eb5749d2fa93cc9c029d40e4b2f7dc724
JackFirth <jackhfirth@gmail.com>
2015-02-22 20:45:43 -0800

    Add export-count tests

--- doc-coverage/export-count.rkt
+++ doc-coverage/export-count.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require "export-lists.rkt")
+(require rackunit
+         "export-lists.rkt")
 
 (provide module-num-exports
          module-num-documented-exports
@@ -11,6 +12,14 @@
 (define module-num-documented-exports (compose length module->documented-exported-names))
 (define module-num-undocumented-exports (compose length module->undocumented-exported-names))
 
+(module+ test
+  (check-eqv? (module-num-exports 'racket/match) 28)
+  (check-eqv? (module-num-documented-exports 'racket/match) 27)
+  (check-eqv? (module-num-undocumented-exports 'racket/match) 1))
+
 (define (module-documentation-ratio mod)
   (/ (module-num-documented-exports mod)
      (module-num-exports mod)))
+
+(module+ test
+  (check-eqv? (module-documentation-ratio 'racket/match) (/ 27 28)))
