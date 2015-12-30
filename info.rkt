e4119aa0f2d44761eab77ce1b2f8ae2ccf748a72
Jack Firth <jackhfirth@gmail.com>
2015-12-29 22:36:29 -0800

    Add cover-coveralls dependency

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define collection 'multi)
 (define deps '("base" "rackunit-lib" "scribble-lib" "racket-index"))
 (define build-deps '("cover"
+                     "cover-coveralls"
                      "scribble-lib"
                      "rackunit-lib"
                      "racket-doc"))
