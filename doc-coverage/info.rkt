11e35f1742dc6042dc10c28f21e6edcae7ebe9a8
Jack Firth <jackhfirth@gmail.com>
2017-04-16 19:18:20 -0700

    Require Racket 6.4+

--- doc-coverage/info.rkt
+++ doc-coverage/info.rkt
@@ -3,7 +3,7 @@
 (define name "doc-coverage")
 (define scribblings '(("doc-coverage.scrbl" ())))
 (define deps
-  '(("base" #:version "6.1")))
+  '(("base" #:version "6.4")))
 (define raco-commands
   '(("doc-coverage" (submod doc-coverage/raco main)
                     "a code documentation coverage tool"
